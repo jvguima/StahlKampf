bddc3692528940394ab9c9a467238b5fcbc991d4
jvguima <jvguima@yahoo.com.br>
2015-07-10 20:07:06 -0300

    Melhoria no script de grid.scn
    >A poisção do mouse é convertida em posição de tile através de conversão
    para inteiro

--- StahlKampf Godot/grid.gd
+++ StahlKampf Godot/grid.gd
@@ -9,11 +9,15 @@ var cell_size = 80
 func _input(ev):
    # Mouse in viewport coordinates
 	if (ev.type==InputEvent.MOUSE_BUTTON):
-		print("Mouse Click/Unclick at: ",ev.pos/80)
-	elif (ev.type==InputEvent.MOUSE_MOTION):
-		print("Mouse Motion at: ",ev.pos/80)
 	
-	var tileID = get_node("TileMap").get_cell(ev.pos.x/80,ev.pos.y/80)
+		var posX = int(ev.pos.x/cell_size)
+		var posY = int(ev.pos.y/cell_size)
+		
+		print("Mouse Click/Unclick at: ",posX, " ", posY)
+	elif (ev.type==InputEvent.MOUSE_MOTION):
+		print("Mouse Motion at: ",posX, " " ,posY)
+		
+	var tileID = get_node("TileMap").get_cell(posX,posY)
    
 	print(tileID)
    # Print the size of the viewport
@@ -23,13 +27,6 @@ func _input(ev):
 func _ready():
 	set_process_input(true)
 	# Initialization here
-	
-	var tilePosX = -get_node("guy").get_pos().x / cell_size
-	var tilePosY = -get_node("guy").get_pos().y / cell_size
-	
-	var tileID = get_node("TileMap").get_cell(tilePosX,tilePosY)
-   
-	print(tileID)
 	
 	#get_node("Label").set_text(y)
 	pass
