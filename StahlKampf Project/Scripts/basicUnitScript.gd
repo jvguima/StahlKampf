d9a02fe76e637cb5741845ee0121d87ab6a3e9ba
Arsart <arsbr.souza@gmail.com>
2015-07-27 21:53:39 -0300

    Adicionado um possivel preview de que quadrados podem ser andados
    Está dentro do comentário. Essa build também traz uma correção do
    terreno. Peço para que tentem  descobrir porque a função _draw() não
    desenha

--- StahlKampf Project/Scripts/basicUnitScript.gd
+++ StahlKampf Project/Scripts/basicUnitScript.gd
@@ -35,6 +35,7 @@ func is_active():
 
 func terrain(x, y):
 	var tile = get_node("../TileMap").get_cell(x,y)
+	print("Terrain: ",tile)
 	if (tile == 0): 
 		return 2
 	if (tile == 1): 
@@ -51,26 +52,58 @@ func _process(delta):
 		set_process(false)
 	
 	elif(Input.is_action_pressed("move_unit_down")):
+		currentMoves = currentMoves - terrain(coordinates.x,coordinates.y)
 		unit_pos.y+=80
-		currentMoves = currentMoves - terrain(coordinates.x,coordinates.y+1)
 		
 	elif(Input.is_action_pressed("move_unit_up")):
+		currentMoves = currentMoves - terrain(coordinates.x,coordinates.y)
 		unit_pos.y-=80
-		currentMoves = currentMoves - terrain(coordinates.x,coordinates.y-1)
 		#OS.delay_usec(200)
-
 		
 	elif(Input.is_action_pressed("move_unit_left")):
+		currentMoves = currentMoves - terrain(coordinates.x,coordinates.y)
 		unit_pos.x-=80
-		currentMoves = currentMoves - terrain(coordinates.x-1,coordinates.y)
 		#OS.delay_usec(200)
 		
 
 	elif(Input.is_action_pressed("move_unit_right")):
-		unit_pos.x+=80
 		currentMoves = currentMoves - terrain(coordinates.x+1,coordinates.y)
+		unit_pos.x+=80
 		#OS.delay_usec(200)
 		
-	
+	print("Moves: ",currentMoves)
 	set_pos(unit_pos)
+	#_draw()
 	
+	
+	"""
+Alguém depois tente entender porque a função não consegue desenhar
+
+func verify_range():
+	var walkpreview Rect2(get_pos().x,get_pos().y,80,80)
+	var currentMoves = moves
+	var blue Color(0,0,1)
+	var aux = 0
+	coordinates = get_pos()/80
+	
+	while(currentMoves - terrain(coordinates.x,coordinates.y) < 0 or currentMoves == 0):
+		draw_rect(walkpreview, blue)
+		currentMoves-=terrain(coordinates.x,coordinates.y)
+		walkpreview.pos = vector2(walkpreview.x+80*aux, walkpreview.y)
+		aux+=1
+	
+	aux=0
+	while(currentMoves - terrain(coordinates.x,coordinates.y) < 0 or currentMoves == 0):
+		draw_rect(walkpreview, blue)
+		currentMoves-=terrain(walkpreview.x/80,walkpreview.y)
+		walkpreview.pos = vector2(walkpreview.x-80*aux, walkpreview.y)
+	
+
+func _draw():
+	if (active):
+		verify_range()
+
+"""
+
+
+
