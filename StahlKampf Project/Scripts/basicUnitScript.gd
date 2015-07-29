2849072c8034463155831bd7d27f697f777f400c
Arsart <arsbr.souza@gmail.com>
2015-07-29 00:21:12 -0300

    Adição de preview de movimento
    Pode causar um leve lag(espero)

--- StahlKampf Project/Scripts/basicUnitScript.gd
+++ StahlKampf Project/Scripts/basicUnitScript.gd
@@ -1,4 +1,3 @@
-
 extends Node2D
 
 # Propriedades
@@ -35,10 +34,12 @@ func is_active():
 
 func terrain(x, y):
 	var tile = get_node("../TileMap").get_cell(x,y)
-	print("Terrain: ",tile)
+	#print("Terrain: ",tile)
 	if (tile == 0): 
 		return 2
 	if (tile == 1): 
+		return 1
+	else:
 		return 1
 	pass
 
@@ -73,37 +74,29 @@ func _process(delta):
 		
 	print("Moves: ",currentMoves)
 	set_pos(unit_pos)
-	#_draw()
+	
+	update()
 	
 	
-	"""
-Alguém depois tente entender porque a função não consegue desenhar
 
-func verify_range():
-	var walkpreview Rect2(get_pos().x,get_pos().y,80,80)
-	var currentMoves = moves
-	var blue Color(0,0,1)
-	var aux = 0
-	coordinates = get_pos()/80
-	
-	while(currentMoves - terrain(coordinates.x,coordinates.y) < 0 or currentMoves == 0):
-		draw_rect(walkpreview, blue)
-		currentMoves-=terrain(coordinates.x,coordinates.y)
-		walkpreview.pos = vector2(walkpreview.x+80*aux, walkpreview.y)
-		aux+=1
-	
-	aux=0
-	while(currentMoves - terrain(coordinates.x,coordinates.y) < 0 or currentMoves == 0):
-		draw_rect(walkpreview, blue)
-		currentMoves-=terrain(walkpreview.x/80,walkpreview.y)
-		walkpreview.pos = vector2(walkpreview.x-80*aux, walkpreview.y)
-	
+func walk_preview(i, j, remainMoves):
+	if(remainMoves - terrain(i,j) < 0 or remainMoves <= 0):
+		draw_rect(Rect2(i*80-get_pos().x-32, j*80-get_pos().y-32, 80, 80), Color(0,0,1,0.5))
+		return
+	else:
+		remainMoves -= terrain(i,j)
+		draw_rect(Rect2(i*80-get_pos().x-32, j*80-get_pos().y-32, 80, 80), Color(0,0,1,0.5))
+		#left
+		walk_preview(i-1, j, remainMoves)
+		#right
+		walk_preview(i+1, j, remainMoves)
+		#up
+		walk_preview(i, j-1, remainMoves)
+		#down
+		walk_preview(i, j+1, remainMoves)
+
 
 func _draw():
 	if (active):
-		verify_range()
-
-"""
-
-
+		walk_preview(coordinates.x,coordinates.y, currentMoves)
 
